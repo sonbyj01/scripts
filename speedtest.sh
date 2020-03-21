676dc1d1ef0dc9558633590711b079c1520be832
H Son <sonbyj01@gmail.com>
2020-03-20 22:09:59 -0700

    added speedtest script

---
+++ speedtest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install gnupg1 apt-transport-https dirmngr -y
+export INSTALL_KEY=379CE192D401AB61
+export DEB_DISTRO=$(lsb_release -sc)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
+sudo apt-get update
+sudo apt-get install speedtest
+
+echo 'YES' | speedtest
