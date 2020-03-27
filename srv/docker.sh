35a66f7a824aa232b15af98c1e188da525df8cf3
H Son <sonbyj01@gmail.com>
2020-03-26 18:12:43 -0700

    new file

---
+++ srv/docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is made specifically for my personal server setup (Docker)
+# By Henry J Son ( @sonbyj01 )
+# Updated: March 26th, 2020
+
+sudo apt-get update
+sudo apt-get install \\
+    apt-transport-https \\
+    ca-certificates \\
+    curl \\
+    gnupg2 \\
+	software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \\
+   "deb [arch=amd64] https://download.docker.com/linux/debian \\
+   $(lsb_release -cs) \\
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
